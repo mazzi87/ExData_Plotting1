9aab0f974e2140a91a21151cfd26bce254032265
mazzi87 <andreamazzanti8@gmail.com>
2015-12-08 23:49:49 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -17,3 +17,4 @@ with(data_mod, plot(New_date, Voltage, xlab = "datetime", ylab = "Voltage", type
 with(data_mod, plot(New_date, Global_reactive_power, xlab = "datetime", type = "l"))
 dev.copy(png, file = "plot4.png", width = 480, height = 480)
 dev.off()
+par(mfcol = c(1,1))
